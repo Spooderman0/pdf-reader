ae1f8e738ba1747ef7564ecf16f6780d16e9b136
jeannette <a01236226@tec.mx>
2024-04-13 18:43:35 -0600

    CRUD users y docs
    ya entendi

---
+++ backend/routes/docs_routes.py
@@ -0,0 +1,96 @@
+from flask import Blueprint, jsonify, request
+from firebase_admin import firestore
+
+docs_blueprint = Blueprint('docs', __name__)
+users_ref = firestore.client().collection('Users')
+
+    
+#GET DATA FROM ALL DOCS FROM 1 USER
+@docs_blueprint.route('/<user_id>/docs', methods = ['GET'])
+def get_docs(user_id):
+    try:
+        user_ref = users_ref.document(user_id)
+        docs_ref = user_ref.collection('Docs')
+        docs_data = [doc.to_dict() for doc in docs_ref.stream()]
+        return jsonify(docs_data)
+        
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+#GET DATA FROM 1 DOC
+@docs_blueprint.route('/<user_id>/docs/<doc_id>', methods = ['GET'])
+def get_doc(user_id, doc_id):
+    try:
+        user_ref = users_ref.document(user_id)
+        docs_ref = user_ref.collection('Docs')
+        doc_ref = docs_ref.document(doc_id)
+        docs_data = doc_ref.get()
+
+        if docs_data.exists:
+            doc_dict = docs_data.to_dict()
+            return jsonify(doc_dict)
+        else:
+            return jsonify({'error': 'Doc no encontrado'})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+#POST 1 DOC
+@docs_blueprint.route('/<user_id>/docs/add', methods = ['POST'])
+def add_doc(user_id):
+    try:
+        data = request.json
+        user_ref = users_ref.document(user_id)
+        docs_ref = user_ref.collection('Docs')
+        doc_ref = docs_ref.document(data.get('id'))
+        doc_ref.set({
+            'Author': data.get('Author'),
+            'Title': data.get('Title'),
+            'Topic': data.get('Topic')
+        })
+
+        # Crear la colección de common terms para el doc y llenarla dummy o vacia
+        profile_ref = doc_ref.collection('Common Terms')
+        profile_ref.document('emptyAnalysis').set({
+            'Abstract': 'abs', 
+        })
+
+        return jsonify({'message' : 'Documento agregado correctamente'})
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+#UPDATE 1 DOC
+@docs_blueprint.route('/<user_id>/docs/update/<doc_id>', methods = ['PUT'])
+def update_doc(user_id, doc_id):
+    try:
+        data = request.json
+        user_ref = users_ref.document(user_id)
+        doc_ref = user_ref.collection('Docs').document(doc_id)
+
+        if doc_ref.get().exists:
+            doc_ref.update({
+                'Author': data.get('Author'),
+                'Title': data.get('Title'),
+                'Topic': data.get('Topic')
+            })
+            return jsonify({'message': 'Doc actualizado correctamente'})
+        else:
+            return jsonify({'error', 'Doc no encontrado'})
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+#DELETE 1 DOC
+@docs_blueprint.route('/<user_id>/docs/delete/<doc_id>', methods = ['DELETE'])
+def delete_doc(user_id, doc_id):
+    try:
+        user_ref = users_ref.document(user_id)
+        doc_ref = user_ref.collection('Docs').document(doc_id)
+        if doc_ref.get().exists:
+            doc_ref.delete()
+            return jsonify({'message' : 'Doc borrado correctamente'})
+        else:
+            return jsonify({'error': 'Doc no encontrado'})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
--- backend/routes/users_routes.py
+++ backend/routes/users_routes.py
@@ -2,17 +2,14 @@ from flask import Blueprint, jsonify, request
 from firebase_admin import firestore
 from PyPDF2 import PdfReader
 
-# Crear un Blueprint para las rutas relacionadas con la colección 'Users'
 users_blueprint = Blueprint('users', __name__)
-
-# Referencia a la colección 'Users'
 users_ref = firestore.client().collection('Users')
 
-#GET all users (lectura)
-@users_blueprint.route('/get_users')
+
+#GET all users
+@users_blueprint.route('/users', methods = ['GET'])
 def get_users():
     try:
-        # Obtener los datos de la colección 'Users'
         users_data = users_ref.get()
         users_list = [doc.to_dict() for doc in users_data]
         #users_list_json = jsonify(users_list)
@@ -21,28 +18,75 @@ def get_users():
         return jsonify(users_list)
     except Exception as e:
         return jsonify({'error': str(e)})
+    
+#GET 1 user
+@users_blueprint.route('/users/<user_id>', methods = ['GET'])
+def get_user(user_id):
+    try:
+        user_ref = users_ref.document(user_id)
+        user_data = user_ref.get()
+        if user_data.exists:
+            user_dict = user_data.to_dict()
+            return jsonify(user_dict)
+        else:
+            return jsonify({'error': 'Usuario no encontrado'})
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
-#POST 1 user (escritura)
-@users_blueprint.route('/add_user', methods=['POST'])
+#ADD user
+@users_blueprint.route('/adduser', methods = ['POST'])
 def add_user():
     try:
         data = request.json
-        nombre = data.get('Nombre')
-        apellido = data.get('Apellido')
+        user_ref = users_ref.document(data.get('id'))
+        user_ref.set({
+            'username': data.get('username'),
+            'pwd': data.get('pwd')
+        })
 
-        if not nombre or not apellido:
-            return jsonify({'error': 'Se requieren nombre y apellido'})
-        
-        new_user_ref = users_ref.document()
-        new_user_ref.set({
-            'Nombre': nombre,
-            'Apellido': apellido
+        # Crear la colección de perfil (profile) para el usuario
+        profile_ref = user_ref.collection('Profile')
+        profile_ref.document('info').set({
+            'Vocab': 3, #valores default
+            'Theme': "Light"
         })
 
         return jsonify({'message': 'Usuario agregado correctamente'})
     except Exception as e:
         return jsonify({'error': str(e)})
     
+#UPDATE 1 user
+@users_blueprint.route('/update/<user_id>', methods=['PUT'])
+def update_user(user_id):
+    try:
+        data = request.json
+        user_ref = users_ref.document(user_id)
+        if user_ref.get().exists:
+            user_ref.update({
+                'username': data.get('username'),
+                'pwd': data.get('pwd')
+            })
+            return jsonify({'message': 'Usuario actualizado correctamente'})
+        else:
+            return jsonify({'error': 'Usuario no encontrado'})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+#DELETE 1 user
+@users_blueprint.route('/delete/<user_id>', methods = ['DELETE'])
+def delete_user(user_id):
+    try:
+        user_ref = users_ref.document(user_id)
+        if user_ref.get().exists:
+            user_ref.delete()
+            return jsonify({'message': 'Usuario borrado correctamente'})
+        else:
+            return jsonify({'error': 'usuario no encontrado'})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+
+#EXTRACCION DE PDF (pasar a otro lado)
 #Extraer contenido de PDF
 @users_blueprint.route('/extract', methods=['POST'])
 def extract_content():
