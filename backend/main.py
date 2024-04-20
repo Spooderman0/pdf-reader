083249ec1876ea29d322fcdef9376e113ce565fa
AGuzman <a01740787@itesm.mx>
2024-04-20 16:50:00 -0600

    PDF Analysis

--- backend/main.py
+++ backend/main.py
@@ -10,10 +10,11 @@ cred = credentials.Certificate("config/keyapiprueba.json")
 default_app = initialize_app(cred)
 
 # Importar y registrar las rutas
-from routes import users_routes, docs_routes, extract_route
+from routes import users_routes, docs_routes, extract_route, analysis_routes
 app.register_blueprint(users_routes.users_blueprint)
 app.register_blueprint(docs_routes.docs_blueprint)
 app.register_blueprint(extract_route.extract_blueprint)
+app.register_blueprint(analysis_routes.analysis_blueprint)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
