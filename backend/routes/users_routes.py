96fba88882ca651f93444084f4486937276fb4ac
AGuzman <a01740787@itesm.mx>
2024-04-20 15:54:14 -0600

    Revert "home route"
    This reverts commit c27596c43d1e3f1f6ea7bc9e5d56a019b3071111.

--- backend/routes/users_routes.py
+++ backend/routes/users_routes.py
@@ -68,13 +68,7 @@ def update_password():
     except Exception as e:
         return jsonify({'error': str(e)})
 
-#Wakey wakey
-@users_blueprint.route('/', methods = ['GET'])
-def update_password():
-    try:
-        return jsonify({'message': 'API is up and running!'})
-    except Exception as e:
-        return jsonify({'error': str(e)})
+
 
 
 #GET all users
