4e90a64f70827e9cb700703755bcfe6090ce02cc
MasterZendika <125993067+MasterZendika@users.noreply.github.com>
2024-04-24 05:28:41 -0600

    Scroll y
    Error de Scroll

--- src/views/MainFrida.js
+++ src/views/MainFrida.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Navbar from '../Components/Navbar'; // Ajusta la ruta según la ubicación real de Navbar.js
-import ChatBox from '../Components/ChatBox';
+import Navbar from '../Components/Navbar';
+import ChatBot from '../Components/ChatBox';
+import ConversationHistory from '../Components/ConversationHistory';
+
 function MainFrida() {
   return (
-    <div>
+    <div style={{ overflowY: 'hidden' }}> {/* Aquí establecemos overflowY: hidden para ocultar el scroll vertical */}
       <Navbar /> 
-      <ChatBox onMessageSent={(message) => console.log(message)} />
+      <ChatBot onMessageSent={(message) => console.log(message)} /> 
+      <ConversationHistory />
     </div>
   );
 }
