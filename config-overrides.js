aa618a4515b04c7d5989eed632d6cad65bcbdcfc
jeannette <a01236226@tec.mx>
2024-04-30 20:57:13 -0600

    leer descrip.
    arreglé su mierdero, aprendan a importar una foto a react: https://youtu.be/MRbqLWAwqko?si=dcaOMk9efrk6dDD5

--- config-overrides.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const { override, addWebpackAlias } = require('customize-cra');
-
-module.exports = override(
-  addWebpackAlias({
-    '@Images': path.resolve(__dirname, 'src/Images/') // Cambiado de '@images' a '@Images'
-  })
-);
