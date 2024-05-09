690ec222b87d622d264d6e194d5d43b4a5a67d7b
Guillermo García <a01620676@tec.mx>
2024-05-09 14:47:36 -0600

    Chat box mejorao

--- src/Components/ConversationHistory.js
+++ src/Components/ConversationHistory.js
@@ -25,7 +25,7 @@ const ConversationHistory = () => {
   ];
   return (
     <div className="flex items-center h-screen justify-left"> {/* Adjusted to fill parent container */}
-      <div className="absolute inset-0" style={{ marginLeft: '2rem', marginRight: '1350px', marginTop: '160px' }}> {/* Adjust for specific positioning */}
+      <div className="container bg-gray-100 rounded-[12px] shadow-lg h-dvh p-8 flex flex-col w-1/4 absolute top-2/3 right-11/16 transform -translate-y-1/2" style={{ marginLeft: '40px' }}>
         <div className="container bg-gray-100 rounded-[12px] shadow-lg w-full h-full flex flex-col justify-start items-center overflow-y-auto overflow-x-hidden">
           <div className="text-black font-bold text-2xl mb-4 bg-gray-100" style={{ width: '100%', padding: '10px 0' }}>Conversation History</div>
           <Scrollbars autoHide>
