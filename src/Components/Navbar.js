5bdbf7cf8f3f5d0d15f37d67d5f5109430fb0651
Guillermo García <a01620676@tec.mx>
2024-05-22 10:15:01 -0600

    Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,12 +4,12 @@ import logo from '../Images/pdf_analyzer_logo2.png';
 import { FaUser, FaCog, FaHistory } from "react-icons/fa";
 import { IoMdAdd } from "react-icons/io";
 import { BACKEND_LINK } from '../utils/constants';
-import { text } from 'd3';
 import { MdInfo } from 'react-icons/md';
 
 const Navbar = () => {
   const [isProfileMenuOpen, setProfileMenuOpen] = useState(false);
   const navigate = useNavigate();
+  const navbarHeight = "10vh"; // Define the height of the navbar
 
   const handleLogout = async () => {
     try {
@@ -37,54 +37,59 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-black text-white py-4 px-6 flex justify-between items-center" style={{height: "10dvh"}}>
-      <div className="flex items-center space-x-4">
-        <img src={logo} alt="PDF Analyst Logo" className="h-10" />
-        <Link to="/" className='hover:text-gray-300'>
-          <span className="font-bold text-xl">PDF Analyst</span>
-        </Link>
-      </div>
-      
-      <div className="flex items-center space-x-4">
-              
-        <Link to="/main" className="hover:text-gray-300">
-          <IoMdAdd className="text-2xl" />
-        </Link>
-
-        <Link to="/about" className="hover:text-gray-300">
-          <MdInfo className="text-2xl" />
-        </Link>
-        <Link to="/history" className="hover:text-gray-300">
-          <FaHistory className="text-2xl" />
-        </Link>
-        
-        <div className="relative">
-          <button onClick={toggleProfileMenu} className="hover:text-gray-300">
-            <FaUser className="text-2xl" />
-          </button>
-          {isProfileMenuOpen && (
-            <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10">
-              <Link
-                to="/settings"
-                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                onClick={() => setProfileMenuOpen(false)}
-              >
-                Ir al perfil
-              </Link>
-              <button
-                onClick={() => {
-                  setProfileMenuOpen(false);
-                  handleLogout();
-                }}
-                className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-              >
-                Cerrar sesión
-              </button>
-            </div>
-          )}
+    <>
+      <nav className="bg-black text-white py-4 px-6 flex justify-between items-center fixed top-0 left-0 right-0 z-50" style={{ height: navbarHeight }}>
+        <div className="flex items-center space-x-4">
+          <img src={logo} alt="PDF Analyst Logo" className="h-10" />
+          <Link to="/" className='hover:text-gray-300'>
+            <span className="font-bold text-xl">PDF Analyst</span>
+          </Link>
         </div>
+        
+        <div className="flex items-center space-x-4">
+                
+          <Link to="/main" className="hover:text-gray-300">
+            <IoMdAdd className="text-2xl" />
+          </Link>
+
+          <Link to="/about" className="hover:text-gray-300">
+            <MdInfo className="text-2xl" />
+          </Link>
+          <Link to="/history" className="hover:text-gray-300">
+            <FaHistory className="text-2xl" />
+          </Link>
+          
+          <div className="relative">
+            <button onClick={toggleProfileMenu} className="hover:text-gray-300">
+              <FaUser className="text-2xl" />
+            </button>
+            {isProfileMenuOpen && (
+              <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10">
+                <Link
+                  to="/settings"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                  onClick={() => setProfileMenuOpen(false)}
+                >
+                  Ir al perfil
+                </Link>
+                <button
+                  onClick={() => {
+                    setProfileMenuOpen(false);
+                    handleLogout();
+                  }}
+                  className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                >
+                  Cerrar sesión
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </nav>
+      <div style={{ marginTop: navbarHeight }}>
+        {/* Aquí irá el contenido de tu página */}
       </div>
-    </nav>
+    </>
   );
 };
 
