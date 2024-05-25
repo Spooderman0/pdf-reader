69c11aad5741658bd2d914fd53f0ec219296459f
jeannette <a01236226@tec.mx>
2024-05-25 16:54:08 -0600

    el titulo
    (que ya estaba)

--- src/views/PDFAnalysis.js
+++ src/views/PDFAnalysis.js
@@ -58,13 +58,13 @@ export const PDFAnalysis = () => {
   };
 
   const title = allData.Title || 'Cargando...';
-  const cleanTitle = title.split('.').slice(0, -1).join('.');
+
 
   return (
     <div className="bg-white w-full flex flex-col" style={{ height: '90vh' }}>
       <div className='flex flex-row'>
         <div className='basis-2/5 items-center flex px-3' style={{ height: '15dvh', marginLeft: '10%' }}>
-          <h4 className="mb-4 text-4xl font-bold">{cleanTitle}</h4>
+          <h4 className="mb-4 text-4xl font-bold">{title}</h4>
         </div>
         <div className='basis-3/5 flex px-3' style={{ height: '15dvh' }}>
           <AnalysisButtons 
