a76e05524e8415b03e8ce946e2baeb4cb319356e
jeannette <a01236226@tec.mx>
2024-05-26 19:49:50 -0600

    figuras ya se ve
    erick me lo quito

--- src/views/PDFAnalysis.js
+++ src/views/PDFAnalysis.js
@@ -93,7 +93,7 @@ export const PDFAnalysis = () => {
           )}
           {currentSection === "figuras" && (
             <div className='flex flex-row'>
-              <SeccionFiguras /> 
+              <SeccionFiguras figuras={allData.Figuras}/> 
             </div>
           )}
 
