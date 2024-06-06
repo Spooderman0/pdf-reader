e99828c85b181acf8d369a77122633832d10e8b7
Guillermo García <a01620676@tec.mx>
2024-06-06 12:07:36 -0600

    Capitulos bien

--- src/Components/SeccionAnalisis.js
+++ src/Components/SeccionAnalisis.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import 'tailwindcss/tailwind.css';
 import { useNavigate } from "react-router-dom";
-import { FaClipboard } from "react-icons/fa";
+import { FaClipboard, FaChevronDown, FaChevronRight } from "react-icons/fa";
 import { BACKEND_LINK } from '../utils/constants';
 import Swal from 'sweetalert2';
 
@@ -9,6 +9,8 @@ export const SeccionAnalisis = ({ docId, docURL, summary, raw_text, sectionSumma
   const [vistaPreTab, setVistaPreTab] = useState('file');
   const [selectedTab, setSelectedTab] = useState('Resumen');
   const [sectionSummaries, setSectionSummaries] = useState();
+  const [expandedSections, setExpandedSections] = useState([]);
+  const [loadingSections, setLoadingSections] = useState([]);
   let authors = [];
   let authorsList;
 
@@ -42,8 +44,9 @@ export const SeccionAnalisis = ({ docId, docURL, summary, raw_text, sectionSumma
   };
 
   const handleSectionClick = async (sectionIndex) => {
-    try {
-      if (!sectionSummaries[sectionIndex]["summary"]) {
+    if (!sectionSummaries[sectionIndex]["summary"]) {
+      setLoadingSections(prev => [...prev, sectionIndex]);
+      try {
         const response = await fetch(`${BACKEND_LINK}/chatbot/${docId}/sectionSummary/${sectionIndex}`, {
           method: 'GET',
           headers: {
@@ -62,10 +65,21 @@ export const SeccionAnalisis = ({ docId, docURL, summary, raw_text, sectionSumma
         let newSectionSummaries = [...sectionSummaries];
         newSectionSummaries[sectionIndex]["summary"] = sectionSummary;
         setSectionSummaries(newSectionSummaries);
+      } catch (error) {
+        console.error(`Error al obtener los datos de sección: ${sectionIndex}`, error);
+      } finally {
+        setLoadingSections(prev => prev.filter(i => i !== sectionIndex));
       }
-    } catch (error) {
-      console.error(`Error al obtener los datos de sección: ${sectionIndex}`, error);
     }
+    toggleSection(sectionIndex);
+  };
+
+  const toggleSection = (index) => {
+    setExpandedSections(prev =>
+      prev.includes(index)
+        ? prev.filter(i => i !== index)
+        : [...prev, index]
+    );
   };
 
   if (author) {
@@ -184,22 +198,23 @@ export const SeccionAnalisis = ({ docId, docURL, summary, raw_text, sectionSumma
             <div>
               {sectionSummaries.map((section, index) => (
                 <div key={index} className="mb-4">
-                  {sectionSummaries[index]["summary"] ? (
-                    <p
-                      className="text-xl font-semibold text-black hover:underline focus:outline-none"
-                      onClick={() => handleSectionClick(index)}
-                    >
-                      {section.title}
-                    </p>
-                  ) : (
-                    <button
-                      className="text-xl font-semibold text-blue-500 hover:underline focus:outline-none"
-                      onClick={() => handleSectionClick(index)}
-                    >
-                      {section.title}
-                    </button>
+                  <div
+                    className="flex justify-between items-center text-xl font-semibold text-black hover:underline focus:outline-none cursor-pointer"
+                    onClick={() => handleSectionClick(index)}
+                  >
+                    <p>{section.title}</p>
+                    {loadingSections.includes(index) ? (
+                      <svg aria-hidden="true" className="inline w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                      </svg>
+                    ) : (
+                      expandedSections.includes(index) ? <FaChevronDown /> : <FaChevronRight />
+                    )}
+                  </div>
+                  {expandedSections.includes(index) && !loadingSections.includes(index) && (
+                    <p>{section.summary}</p>
                   )}
-                  <p>{section.summary}</p>
                 </div>
               ))}
             </div>
@@ -208,7 +223,7 @@ export const SeccionAnalisis = ({ docId, docURL, summary, raw_text, sectionSumma
         <div className="card bg-gray-100 p-3 border-0 shadow-md" style={{ height: "30dvh" }}>
           <h5 className="mb-4 text-2xl font-bold">Referencia</h5>
           <div className="flex justify-between items-center">
-            <div style={{overflowY:'auto'}}>
+            <div style={{ overflowY: 'auto' }}>
               <p>{referencia}</p>
             </div>
             <button
