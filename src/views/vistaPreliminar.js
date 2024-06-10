bb01b8248ae7904083ae0f8f840ca79b646f625d
Erick Schiller <erickschiller@hotmail.com>
2024-06-10 13:59:12 -0600

    Update vistaPreliminar.js

--- src/views/vistaPreliminar.js
+++ src/views/vistaPreliminar.js
@@ -1,21 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom'
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { IoIosArrowBack } from "react-icons/io";
 
 export const VistaPreliminar = () => {
     const location = useLocation();
+    const navigate = useNavigate();
     const docURL = location.state && location.state.docURL;
-    //const { docURL } = location.state;
 
-    //console.log('el url que se debe de ver en vista preliminar es', docURL)
+    const handleBackClick = () => {
+        navigate(-1);
+    };
+
     return (
-        <div>
-            <iframe
-            id="inlineFrameExample"
-            title="Inline Frame Example"
-            width="100%"
-            height="1000dv"
-            src={docURL}>
-            </iframe>
+        <div className='bg-black flex flex-row pt-5'>
+            <div className='flex justify-center items-start' style={{width: "5%"}}>
+                <button className='text-white text-2xl' onClick={handleBackClick}>
+                    <IoIosArrowBack className='' />
+                </button>
+                
+            </div>
+            <div style={{width: "90%"}}>
+                <iframe
+                    id="inlineFrameExample"
+                    title="Inline Frame Example"
+                    width="100%"
+                    height="1000px"
+                    src={docURL}>
+                </iframe>
+
+            </div>
         </div>
     );
 }
